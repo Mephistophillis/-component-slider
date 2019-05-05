634c62ad27d4c0e4fead8179b23b1c418dc56891
Mephistophillis <woodenjester@gmail.com>
2019-05-05 18:12:38 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Slider from './Slider'
+
+class App extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        <Slider />
+      </Wrapper>
+    )
+  }
+}
+
+const Wrapper = styled.div``
+
+export default App
